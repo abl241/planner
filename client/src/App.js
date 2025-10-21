507088e1546f03c840b76275d25d1b96c054b781
abl241 <alexanderblee123@gmail.com>
2025-10-21 16:44:51 -0400

    Organized directories - separated auth pages from dash pages. CSS for AuthForm

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* auth routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         {/* redirect to login by default */}
         <Route path="*" element={<Navigate to="/login" replace />} />
+
+        {/* dashboard routes */}
+        
       </Routes>
     </Router>
   );
